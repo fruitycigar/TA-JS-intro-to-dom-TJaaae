7a75c46794d793f65adf9f55fc88b99b68f424a2
fruitycigar <shruthisagar@SHODAN.local>
2021-12-17 16:14:08 +0530

    i could talk the talk, but couldn't dom the dom

--- block-BJaadz/code/script.js
+++ block-BJaadz/code/script.js
@@ -1,11 +1,12 @@
 // Select the h1 element and store it in a variable named heading.
-
+let heading = document.querySelector('h1');
 // Check the typeof heading and log it.
-
+typeof heading;
+console.log(typeof heading);
 // Change the color of heading to black.
-
+heading.style.color = "black";
 // Select all the hr elements and store it in a variable named allHrs using querySelectorAll
-
+let allHrs = document.querySelectorAll('hr');
 /* 
 Convert the NodeList returned by querySelectorAll to Array using Array.from() or spread operator and store it in allHrsArray
 
@@ -14,7 +15,7 @@ Array.from converts an array kind of data into array so we can use methods like 
 HINT:
 let allHrsArray = Array.from(allHrs)
 */
-
+let allHrsArray = Array.from(allHrs);
 // Set the border of the all the hr elements to "1px solid tomato"
 
 // Change the background of all the hr to "antiquewhite" using for of loop.
