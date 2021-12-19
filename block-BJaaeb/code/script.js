6e81c28691f690b41667ecbf9ea4b854679fe407
fruitycigar <shruthisagar@SHODAN.local>
2021-12-19 13:35:03 +0530

    manipulating dominic toretto didn't work in the first try

--- block-BJaaeb/code/script.js
+++ block-BJaaeb/code/script.js
@@ -5,7 +5,9 @@ default value to be "text" and return the input element inside label. (create it
 
 */
 
-// Your code goes here
+function createInputElm (label, type = "text") {
+  let label = document.createElement
+}
 
 // TEST
 createInputElm('Your name'); //<label>Your name: <input type="text"></label>
