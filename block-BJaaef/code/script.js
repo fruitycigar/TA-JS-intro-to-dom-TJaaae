d3d375161706f07c5390b11845baff5b54702b9b
fruitycigar <shruthisagar@SHODAN.local>
2021-12-21 16:28:15 +0530

    Ankit's favorite show strikes again

---
+++ block-BJaaef/code/script.js
@@ -0,0 +1,19 @@
+    <div class="char-container">   
+        <div class="flex center">
+            <img src="https://raw.githubusercontent.com/nnnkit/json-data-collections/master/game-of-thrones/game-of-thrones-eddard-stark.jpg" alt="eddard">
+          <h2>Eddard "Ned" Stark</h2>
+        </div>
+          <p>Lord of Winterfell - Warden of the North - Hand of the King - Married to Catelyn (Tully) Stark</p>
+          <button>Learn More!</button>
+    </div>
+
+
+
+got.houses.forEach((char) => {
+    let div1 = document.createElement('div');
+    let div2 = document.createElement('div');
+    let img = document.createElement('img');
+    let h2 = document.createElement('h2');
+    let p = document.createElement('p');
+    let button = document.createElement('button');
+})
